3d35742df2f19635a629b17df9c70775e11ae1ca
AngelR <57645824+Sirdarko@users.noreply.github.com>
2025-10-06 20:30:26 -0300

    Add files via upload

---
+++ DatosPaisesV2/src/Modelo/Pais.java
@@ -0,0 +1,52 @@
+package Modelo;
+
+public class Pais {
+
+    protected int codigo;
+    protected String nombrePais;
+    protected String continente;
+    protected int poblacion;
+
+    public Pais() {
+    }
+
+    public Pais(int codigo, String nombrePais, String continente, int poblacion) {
+        this.codigo = codigo;
+        this.nombrePais = nombrePais;
+        this.continente = continente;
+        this.poblacion = poblacion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombrePais() {
+        return nombrePais;
+    }
+
+    public void setNombrePais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
+
+    public String getContinente() {
+        return continente;
+    }
+
+    public void setContinente(String continente) {
+        this.continente = continente;
+    }
+
+    public int getPoblacion() {
+        return poblacion;
+    }
+
+    public void setPoblacion(int poblacion) {
+        this.poblacion = poblacion;
+    }
+
+}
---
+++ DatosPaisesV2/src/Vista/Main.java
@@ -0,0 +1,10 @@
+package Vista;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Vista vista = new Vista();
+        vista.setVisible(true);
+    }
+}
