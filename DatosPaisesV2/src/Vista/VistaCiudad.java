05ca6e0ff4d40bbfa76060b0007ac7b55312c148
eloy <pendeguin12@gmail.com>
2025-10-15 21:27:40 -0300

    se hace funcional la vistaMenuPrincipal

--- DatosPaisesV2/src/Vista/VistaCiudad.java
+++ DatosPaisesV2/src/Vista/VistaCiudad.java
@@ -8,14 +8,14 @@ package Vista;
  *
  * @author juan_
  */
-public class VistaCuidad extends javax.swing.JFrame {
+public class VistaCiudad extends javax.swing.JFrame {
     
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(VistaCuidad.class.getName());
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(VistaCiudad.class.getName());
 
     /**
      * Creates new form VistaCuidad
      */
-    public VistaCuidad() {
+    public VistaCiudad() {
         initComponents();
     }
 
@@ -66,7 +66,7 @@ public class VistaCuidad extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new VistaCuidad().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new VistaCiudad().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
